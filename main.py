d3e660d65f828c8d965ce2bffd6486f75eed72a8
Shamiivan <shamiivan@gmail.com>
2023-09-30 08:37:49 -0400

    Make a module of handtracking

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import cv2
 import mediapipe as mp
 import serial
 import struct
-from pySerialTransfer import pySerialTransfer as txfer
-import time 
-## Setup arduino serial communication
 
 # Create objects
 mpHands = mp.solutions.hands
@@ -30,24 +27,15 @@ while True:
         for handLms in results.multi_hand_landmarks:
             # For every hand landmark on a hand
             for id, lm in enumerate(handLms.landmark):
-                # Get landmark x, y, and z coordinates (normalized to range [0.0, 1.0])
-                h, w, c = img.shape
-                cx, cy, cz = int(lm.x * w), int(lm.y * h), int(lm.z * 100) # Convert to pixel and depth coordinates
-                print(f"2D Coordinates: ({cx}, {cy}), 3D Depth: {cz}")
+                if id == 9: # For example, to highlight thumb tip only. Remove for loop to highlight all points
 
-                # send the data to arduino 
-
-                send_size = 0
-                
-                ###################################################################
-                # Send a list
-                ###################################################################
-                list_ = [cx, cy, cx]
-                list_size = link.tx_obj(list_)
-                send_size += list_size
-                if id == 4: # For example, to highlight thumb tip only. Remove for loop to highlight all points
+                    # Get landmark x, y, and z coordinates (normalized to range [0.0, 1.0])
+                    h, w, c = img.shape
+                    cx, cy, cz = int(lm.x * w), int(lm.y * h), int(lm.z * 100) # Convert to pixel and depth coordinates
+                    print(f"2D Coordinates: ({id} -- {cx}, {cy}),  Depth: {cz}")
+                    print(f"Dimensions : {h} - {w}")
+                    print(f"Raw coordinates : {lm.x} -{lm.y}")
                     cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED) # Draw circle at hand landmark
-
             # Draw the hand landmarks and connections
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
@@ -63,3 +51,6 @@ while True:
 
 # Release the webcam and destroy windows
 cap.release()
+
+if __name__ == "__main__":
+    pass
